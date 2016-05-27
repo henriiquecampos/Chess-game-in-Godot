3547983bf01317d038fae592fb317d93e86933dc
Henrique Campos <henriiquecampos@gmail.com>
2016-05-27 12:21:42 -0300

    New logic created, initialized the new base piece

--- Game/scripts/base_piece.gd
+++ Game/scripts/base_piece.gd
@@ -1,9 +1,18 @@
 
 extends Area2D
 
-# member variables here, example:
-# var a=2
-# var b="textvar"
+#Base logic booleans
+var is_selected = false
+var is_on_top = false
+var mouse_click = false
+var mouse_cliked = false
+
+#Cell movement logic
+var movable_cells = []
+var self_cell = null
+var selected_cell = null
+
+#Nodes needed
 
 func _ready():
 	# Called every time the node is added to the scene.
