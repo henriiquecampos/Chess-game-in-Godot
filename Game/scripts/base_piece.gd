2d64a8230f94674cc502952f4db1d38a2cda2e77
Henrique Campos <henriiquecampos@gmail.com>
2016-05-27 13:02:52 -0300

    Selection logic working

--- Game/scripts/base_piece.gd
+++ Game/scripts/base_piece.gd
@@ -22,8 +22,23 @@ func _ready():
 func _process(delta):
 	mouse_click = Input.is_action_pressed("mouse")
 	if mouse_click and not mouse_clicked:
-		print("Clicked!")
+		if is_on_top:
+			select_piece()
 		
 	mouse_clicked = mouse_click
 
 
+func select_piece():
+	#Toggle between selected and unselected
+	if is_selected == false:
+		is_selected = true
+	else:
+		is_selected = false
+
+#Verifies which piece is being clicked
+func _on_base_piece_mouse_enter():
+	is_on_top = true
+
+
+func _on_base_piece_mouse_exit():
+	is_on_top = false
