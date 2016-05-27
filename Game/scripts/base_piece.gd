c797d5e364a895b24c331d3f8f8ddcf6657bd536
Henrique Campos <henriiquecampos@gmail.com>
2016-05-27 12:32:15 -0300

    Mouse clicking logic setup

--- Game/scripts/base_piece.gd
+++ Game/scripts/base_piece.gd
@@ -5,7 +5,7 @@ extends Area2D
 var is_selected = false
 var is_on_top = false
 var mouse_click = false
-var mouse_cliked = false
+var mouse_clicked = false
 
 #Cell movement logic
 var movable_cells = []
@@ -13,10 +13,17 @@ var self_cell = null
 var selected_cell = null
 
 #Nodes needed
+#onready var board = get_node("/root/main_scene/board")
+#onready var controller = get_node("/root/main_scene")
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+	set_process(true)
+	
+func _process(delta):
+	mouse_click = Input.is_action_pressed("mouse")
+	if mouse_click and not mouse_clicked:
+		print("Clicked!")
+		
+	mouse_clicked = mouse_click
 
 
