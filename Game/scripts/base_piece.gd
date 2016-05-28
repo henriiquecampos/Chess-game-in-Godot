24b39e75735d4aa8443c5de079d06ddc704f744a
Henrique Campos <henriiquecampos@gmail.com>
2016-05-27 23:18:22 -0300

    Move logic working
    Have to fix the need of double select an unity to calc it's movable cells

--- Game/scripts/base_piece.gd
+++ Game/scripts/base_piece.gd
@@ -1,12 +1,14 @@
 
 extends Area2D
 
+export var which_piece = "Piece"
+
 #Base logic booleans
 var is_selected = false
 var is_on_top = false
 var mouse_click = false
 var mouse_clicked = false
-
+var already_moved = false
 #Cell movement logic
 var movable_cells = []
 var self_cell = null
@@ -14,7 +16,7 @@ var selected_cell = null
 
 #Nodes needed
 onready var board = get_node("/root/main_scene/board")
-#onready var controller = get_node("/root/main_scene")
+onready var controller = get_node("/root/main_scene")
 
 func _ready():
 	set_process(true)
@@ -23,16 +25,16 @@ func _ready():
 func _process(delta):
 	mouse_click = Input.is_action_pressed("mouse")
 	if mouse_click and not mouse_clicked:
-		calc_cell("pawn")
 		if is_on_top:
 			select_piece()
+		else:
+			move_to()
 	mouse_clicked = mouse_click
 ####################################################
 
 #Verifies which piece is being clicked
 func _on_base_piece_mouse_enter():
 	is_on_top = true
-
 
 func _on_base_piece_mouse_exit():
 	is_on_top = false
@@ -42,16 +44,29 @@ func select_piece():
 	#Toggle between selected and unselected
 	if is_selected == false:
 		is_selected = true
-		print(is_selected)
+		calc_cell_white(which_piece)
 	else:
 		is_selected = false
-		print(is_selected)
 ####################################################
 
-func calc_cell(piece):
+func calc_cell_white(piece):
 	self_cell = board.world_to_map(self.get_pos())
-	selected_cell = board.world_to_map(get_viewport().get_mouse_pos())
+	#Checks if is selected and calc the movement according
+	#to the type of cell being selected
 	if is_selected:
-		print(self_cell)
-		print(selected_cell)
-	
+		if piece == "pawn":
+			if already_moved:
+				movable_cells.append(Vector2(self_cell.x, self_cell.y -1))
+			else:
+				movable_cells.append(Vector2(self_cell.x, self_cell.y -1))
+				movable_cells.append(Vector2(self_cell.x, self_cell.y -2))
+				already_moved = true
+			print(movable_cells)
+####################################################
+
+func move_to():
+	selected_cell = board.world_to_map(get_viewport().get_mouse_pos())
+	if not selected_cell == self_cell:
+		if selected_cell in movable_cells:
+			set_global_pos(board.map_to_world(selected_cell))
+			movable_cells.clear()
