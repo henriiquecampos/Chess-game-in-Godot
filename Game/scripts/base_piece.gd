43cbf982d49e695dc8559288d165c3de407ea255
Henrique Campos <henriiquecampos@gmail.com>
2016-06-01 21:39:15 -0300

    Implemented the inverted(?) inheritation
    Created the Pawn piece based on the Base_piece node, the invert inheritance is because the base is a child of the Pawn piece, which should inherit it

--- Game/scripts/base_piece.gd
+++ Game/scripts/base_piece.gd
@@ -1,8 +1,6 @@
 
 extends Area2D
 
-export var which_piece = "Piece"
-
 #Base logic booleans
 var is_selected = false
 var is_on_top = false
@@ -11,13 +9,14 @@ var mouse_clicked = false
 var already_moved = false
 #Cell movement logic
 var movable_cells = []
-var self_cell = null
+var parent_cell = null
 var selected_cell = null
 
 #Capture required variables
 var opponent_pieces
 
 #Nodes needed
+onready var parent = get_node("..")
 onready var board = get_node("/root/main_scene/board")
 onready var controller = get_node("/root/main_scene")
 
@@ -38,68 +37,46 @@ func _process(delta):
 #Verifies which piece is being clicked
 func _on_base_piece_mouse_enter():
 	is_on_top = true
-
+	print("is on")
 func _on_base_piece_mouse_exit():
 	is_on_top = false
+	print("is out")
 ###################################################
 
 func select_piece():
 	#Toggle between selected and unselected
-	if is_selected == false and self.is_in_group(str(controller.turn)):
+	if is_selected == false and parent.is_in_group(str(controller.turn)):
 		is_selected = true
+		print("it is selected")
 		if controller.turn == "white":
-			calc_cell_white(which_piece)
+			parent.calc_cell_white(parent.which_piece)
 		elif controller.turn == "black":
-			calc_cell_black(which_piece)
+			parent.calc_cell_black(parent.which_piece)
 	else:
 		is_selected = false
-####################################################
-
-func calc_cell_white(piece):
-	self_cell = board.world_to_map(self.get_pos())
-	#Checks if is selected and calc the movement according
-	#to the type of cell being selected
-	if is_selected:
-		#Calculation for the pawn piece
-		if piece == "pawn":
-			if already_moved:
-				movable_cells.append(Vector2(self_cell.x, self_cell.y -1))
-			else:
-				movable_cells.append(Vector2(self_cell.x, self_cell.y -1))
-				movable_cells.append(Vector2(self_cell.x, self_cell.y -2))
-				already_moved = true
-
-func calc_cell_black(piece):
-	self_cell = board.world_to_map(self.get_pos())
-	#Checks if is selected and calc the movement according
-	#to the type of cell being selected
-	if is_selected:
-		#Calculation for the pawn piece
-		if piece == "pawn":
-			if already_moved:
-				movable_cells.append(Vector2(self_cell.x, self_cell.y +1))
-			else:
-				movable_cells.append(Vector2(self_cell.x, self_cell.y +1))
-				movable_cells.append(Vector2(self_cell.x, self_cell.y +2))
-				already_moved = true
+		print("deselected")
 ####################################################
 
 func move_to():
 	selected_cell = board.world_to_map(get_viewport().get_mouse_pos())
-	if not selected_cell == self_cell:
+	if not selected_cell == parent_cell:
 		if selected_cell in movable_cells:
 			var where_cap = capture()
 			if selected_cell in where_cap:
 				opponent_pieces[where_cap.find(selected_cell)].queue_free()
-			set_global_pos(board.map_to_world(selected_cell))
+			parent.set_global_pos(board.map_to_world(selected_cell))
 			movable_cells.clear()
 			opponent_pieces.clear()
 			is_selected = false
 			controller.toggle_turn()
+			print("moving")
 ####################################################
 
 func capture():
-	opponent_pieces = get_node("/root/main_scene/player_black").get_children()
+	if controller.turn == "white":
+		opponent_pieces = get_node("/root/main_scene/player_black").get_children()
+	else:
+		opponent_pieces = get_node("/root/main_scene/player_white").get_children()
 	var capturable = []
 	for i in range(0, opponent_pieces.size()):
 		capturable.append(board.world_to_map(opponent_pieces[i].get_pos()))
---
+++ Game/scripts/pawn.gd
@@ -0,0 +1,44 @@
+
+extends Node2D
+
+export var which_piece = "Piece"
+onready var base = get_node("base_piece")
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+func calc_cell_white(piece):
+	base.parent_cell = base.board.world_to_map(base.parent.get_pos())
+	#Checks if is selected and calc the movement according
+	#to the type of cell being selected
+	if base.is_selected:
+		#Calculation for the pawn piece
+		if piece == "pawn":
+			print("calculating pawn_white")
+			if base.already_moved:
+				base.movable_cells.append(Vector2(base.parent_cell.x, base.parent_cell.y -1))
+			else:
+				base.movable_cells.append(Vector2(base.parent_cell.x, base.parent_cell.y -1))
+				base.movable_cells.append(Vector2(base.parent_cell.x, base.parent_cell.y -2))
+				base.already_moved = true
+
+func calc_cell_black(piece):
+	base.parent_cell = base.board.world_to_map(base.parent.get_pos())
+	#Checks if is selected and calc the movement according
+	#to the type of cell being selected
+	if base.is_selected:
+		#Calculation for the pawn piece
+		if piece == "pawn":
+			print("calculating pawn_black")
+			if base.already_moved:
+				base.movable_cells.append(Vector2(base.parent_cell.x, base.parent_cell.y +1))
+			else:
+				base.movable_cells.append(Vector2(base.parent_cell.x, base.parent_cell.y +1))
+				base.movable_cells.append(Vector2(base.parent_cell.x, base.parent_cell.y +2))
+				base.already_moved = true
+
+
+func _on_pawn_exit_tree():
+	print("HEY i WAS CAPTURED!")
